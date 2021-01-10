9b7bfe016e0336c437fb1337e95c23242c9b98b3
João Carlos Silva de Souza <jocasiso@gmail.com>
2021-01-10 15:40:37 -0300

    Playing with methods and paths(Calculator)

---
+++ RestWithASP_NET5/RestWithASP_NET5/Controllers/CalculatorController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASP_NET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(isNumeric(firstNumber) && isNumeric(secondNumber)){
+                var sum = convertToDecimal(firstNumber) + convertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+           return BadRequest("Invalid imput");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if(isNumeric(firstNumber) && isNumeric(secondNumber)){
+                var sum = convertToDecimal(firstNumber) - convertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+           return BadRequest("Invalid imput");
+        }
+
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult Mult(string firstNumber, string secondNumber)
+        {
+            if(isNumeric(firstNumber) && isNumeric(secondNumber)){
+                var sum = convertToDecimal(firstNumber) * convertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+           return BadRequest("Invalid imput");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if(isNumeric(firstNumber) && isNumeric(secondNumber) && convertToDecimal(secondNumber) != 0 ){
+                
+                var sum = convertToDecimal(firstNumber) + convertToDecimal(secondNumber);
+                
+                return Ok(sum.ToString());
+            }
+           return BadRequest("Invalid imput");
+        }
+
+        private decimal convertToDecimal(string strtNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strtNumber, out decimalValue)){
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool isNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber,System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+        
+            return isNumber;
+        }
+    }
+}
